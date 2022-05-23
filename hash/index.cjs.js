94a99b7f2a4ebf87be28e8c630457145133d43d9
LE VAN THANH <zero.lethanh@gmail.com>
2022-05-23 12:52:24 +0700

    build

--- hash/index.cjs.js
+++ hash/index.cjs.js
@@ -5,65 +5,57 @@ var __getOwnPropNames = Object.getOwnPropertyNames;
 var __getProtoOf = Object.getPrototypeOf;
 var __hasOwnProp = Object.prototype.hasOwnProperty;
 var __export = (target, all) => {
-    for (var name in all)
-        __defProp(target, name, {get: all[name], enumerable: true});
+  for (var name in all)
+    __defProp(target, name, { get: all[name], enumerable: true });
 };
 var __copyProps = (to, from, except, desc) => {
-    if (from && typeof from === 'object' || typeof from === 'function') {
-        for (let key of __getOwnPropNames(from))
-            if (!__hasOwnProp.call(to, key) && key !== except)
-                __defProp(to, key, {
-                    get: () => from[key],
-                    enumerable: !(desc = __getOwnPropDesc(from, key)) ||
-                        desc.enumerable,
-                });
-    }
-    return to;
+  if (from && typeof from === "object" || typeof from === "function") {
+    for (let key of __getOwnPropNames(from))
+      if (!__hasOwnProp.call(to, key) && key !== except)
+        __defProp(to, key, { get: () => from[key], enumerable: !(desc = __getOwnPropDesc(from, key)) || desc.enumerable });
+  }
+  return to;
 };
-var __toESM = (mod, isNodeMode, target) => (target = mod != null ? __create(
-    __getProtoOf(mod)) : {}, __copyProps(
-    isNodeMode || !mod || !mod.__esModule ? __defProp(target, 'default',
-        {value: mod, enumerable: true}) : target, mod));
-var __toCommonJS = (mod) => __copyProps(
-    __defProp({}, '__esModule', {value: true}), mod);
+var __toESM = (mod, isNodeMode, target) => (target = mod != null ? __create(__getProtoOf(mod)) : {}, __copyProps(isNodeMode || !mod || !mod.__esModule ? __defProp(target, "default", { value: mod, enumerable: true }) : target, mod));
+var __toCommonJS = (mod) => __copyProps(__defProp({}, "__esModule", { value: true }), mod);
 var hash_exports = {};
 __export(hash_exports, {
-    setHash: () => setHash,
-    toHashObject: () => toHashObject,
-    toHashString: () => toHashString,
+  setHash: () => setHash,
+  toHashObject: () => toHashObject,
+  toHashString: () => toHashString
 });
 module.exports = __toCommonJS(hash_exports);
-var import_isEmpty = __toESM(require('lodash/isEmpty'));
-var import_isPlainObject = __toESM(require('lodash/isPlainObject'));
+var import_isEmpty = __toESM(require("lodash/isEmpty"));
+var import_isPlainObject = __toESM(require("lodash/isPlainObject"));
 const toHashObject = (hash) => {
-    if ((0, import_isEmpty.default)(hash))
-        return null;
-    hash = hash.replace('#', '');
-    return hash.split(';').map((h) => h.split(':')).reduce((obj, curr) => {
-        if (!curr[1])
-            return obj;
-        const k = curr[0];
-        obj[k] = curr[1];
-        return obj;
-    }, {});
+  if ((0, import_isEmpty.default)(hash))
+    return null;
+  hash = hash.replace("#", "");
+  return hash.split(";").map((h) => h.split(":")).reduce((obj, curr) => {
+    if (!curr[1])
+      return obj;
+    const k = curr[0];
+    obj[k] = curr[1];
+    return obj;
+  }, {});
 };
 const toHashString = (obj) => {
-    if (!(0, import_isPlainObject.default)(obj))
-        return '';
-    let result = '';
-    Object.keys(obj).forEach((k) => {
-        const val = obj[k];
-        result = result + `${k}:${val};`;
-    });
-    return result;
+  if (!(0, import_isPlainObject.default)(obj))
+    return "";
+  let result = "";
+  Object.keys(obj).forEach((k) => {
+    const val = obj[k];
+    result = result + `${k}:${val};`;
+  });
+  return result;
 };
 const setHash = (str) => {
-    const newHash = window.location.hash + ';' + str;
-    window.location.hash = toHashString(toHashObject(newHash));
+  const newHash = window.location.hash + ";" + str;
+  window.location.hash = toHashString(toHashObject(newHash));
 };
 // Annotate the CommonJS export names for ESM import in node:
 0 && (module.exports = {
-    setHash,
-    toHashObject,
-    toHashString,
+  setHash,
+  toHashObject,
+  toHashString
 });
--- hash/index.esm.js
+++ hash/index.esm.js
@@ -1,34 +1,33 @@
-import isEmpty from 'lodash/isEmpty';
-import isPlainObject from 'lodash/isPlainObject';
-
+import isEmpty from "lodash/isEmpty";
+import isPlainObject from "lodash/isPlainObject";
 const toHashObject = (hash) => {
-    if (isEmpty(hash))
-        return null;
-    hash = hash.replace('#', '');
-    return hash.split(';').map((h) => h.split(':')).reduce((obj, curr) => {
-        if (!curr[1])
-            return obj;
-        const k = curr[0];
-        obj[k] = curr[1];
-        return obj;
-    }, {});
+  if (isEmpty(hash))
+    return null;
+  hash = hash.replace("#", "");
+  return hash.split(";").map((h) => h.split(":")).reduce((obj, curr) => {
+    if (!curr[1])
+      return obj;
+    const k = curr[0];
+    obj[k] = curr[1];
+    return obj;
+  }, {});
 };
 const toHashString = (obj) => {
-    if (!isPlainObject(obj))
-        return '';
-    let result = '';
-    Object.keys(obj).forEach((k) => {
-        const val = obj[k];
-        result = result + `${k}:${val};`;
-    });
-    return result;
+  if (!isPlainObject(obj))
+    return "";
+  let result = "";
+  Object.keys(obj).forEach((k) => {
+    const val = obj[k];
+    result = result + `${k}:${val};`;
+  });
+  return result;
 };
 const setHash = (str) => {
-    const newHash = window.location.hash + ';' + str;
-    window.location.hash = toHashString(toHashObject(newHash));
+  const newHash = window.location.hash + ";" + str;
+  window.location.hash = toHashString(toHashObject(newHash));
 };
 export {
-    setHash,
-    toHashObject,
-    toHashString,
+  setHash,
+  toHashObject,
+  toHashString
 };
