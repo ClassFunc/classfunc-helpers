17e607f51951f13f3351a792298e7baf78da9c2e
LE VAN THANH <zero.lethanh@gmail.com>
2022-05-23 15:28:33 +0700

    build

--- hash/index.cjs.js
+++ hash/index.cjs.js
@@ -20,17 +20,17 @@ var __toESM = (mod, isNodeMode, target) => (target = mod != null ? __create(__ge
 var __toCommonJS = (mod) => __copyProps(__defProp({}, "__esModule", { value: true }), mod);
 var hash_exports = {};
 __export(hash_exports, {
-    resetHash: () => resetHash,
-    setHash: () => setHash,
-    toHashObject: () => toHashObject,
-    toHashString: () => toHashString,
+  resetHash: () => resetHash,
+  setHash: () => setHash,
+  toHashObject: () => toHashObject,
+  toHashString: () => toHashString
 });
 module.exports = __toCommonJS(hash_exports);
 var import_isEmpty = __toESM(require("lodash/isEmpty"));
 var import_isPlainObject = __toESM(require("lodash/isPlainObject"));
 const toHashObject = (hash) => {
   if ((0, import_isEmpty.default)(hash))
-      return {};
+    return {};
   hash = hash.replace("#", "");
   return hash.split(";").map((h) => h.split(":")).reduce((obj, curr) => {
     if (!curr[1])
@@ -41,31 +41,30 @@ const toHashObject = (hash) => {
   }, {});
 };
 const toHashString = (obj) => {
-    if (!(0, import_isPlainObject.default)(obj) ||
-        (0, import_isEmpty.default)(obj))
-        return '';
-    let result = '';
-    Object.keys(obj).forEach((k) => {
-        const val = obj[k];
-        result = result + `${k}:${val};`;
-    });
-    return result;
+  if (!(0, import_isPlainObject.default)(obj) || (0, import_isEmpty.default)(obj))
+    return "";
+  let result = "";
+  Object.keys(obj).forEach((k) => {
+    const val = obj[k];
+    result = result + `${k}:${val};`;
+  });
+  return result;
 };
 const setHash = (hash) => {
-    let hashObj;
-    if ((0, import_isPlainObject.default)(hash))
-        hashObj = hash;
-    else if (typeof hash === 'string')
-        hashObj = toHashObject(window.location.hash + ';' + hash);
-    window.location.hash = toHashString(hashObj);
+  let hashObj;
+  if ((0, import_isPlainObject.default)(hash))
+    hashObj = hash;
+  else if (typeof hash === "string")
+    hashObj = toHashObject(window.location.hash + ";" + hash);
+  window.location.hash = toHashString(hashObj);
 };
 const resetHash = (str) => {
-    window.location.hash = str || '';
+  window.location.hash = str || "";
 };
 // Annotate the CommonJS export names for ESM import in node:
 0 && (module.exports = {
-    resetHash,
-    setHash,
-    toHashObject,
-    toHashString,
+  resetHash,
+  setHash,
+  toHashObject,
+  toHashString
 });
